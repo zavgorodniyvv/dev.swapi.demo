98b9385cd6859588c04b731197168824b33497e4
Slava Zavgorodniy <slava.zavgorodniy@radcom.com>
2023-12-17 08:06:34 +0200

    add LRU Cache
    -create LRUCache class
    -create CacheService interface
    -create CacheServiceImpl, implementation of CacheService based on LRUCache
    -add logic to work with cache into PersonServiceImpl

--- src/main/java/com/example/swapidemo/service/PersonServiceImpl.java
+++ src/main/java/com/example/swapidemo/service/PersonServiceImpl.java
@@ -4,8 +4,10 @@ import com.example.swapidemo.exception.SwapiAppException;
 import com.example.swapidemo.model.Person;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,16 +17,30 @@ public class PersonServiceImpl implements PersonService{
     private final HttpService httpService;
     private final URLCreator urlCreator;
     private final ObjectMapper swapiObjectMapper;
+    private final CacheService<String, Person> cacheService;
 
-    public PersonServiceImpl(HttpService httpService, URLCreator urlCreator, ObjectMapper swapiObjectMapper) {
+    public PersonServiceImpl(HttpService httpService, URLCreator urlCreator, ObjectMapper swapiObjectMapper, CacheService<String, Person> cacheService) {
         this.httpService = httpService;
         this.urlCreator = urlCreator;
         this.swapiObjectMapper = swapiObjectMapper;
+        this.cacheService = cacheService;
     }
+
 
     @Override
     public Person getPerson(String id) {
         logger.info("Get request for people with id: {}", id);
+        Person person = getPersonFromCache(id);
+        if (person == null) {
+            logger.info("Person not found in cache. Will get it from SWAPI");
+            person = getPersonFromSwapi(id);
+            cacheService.put(id, person);
+        }
+        logger.info("Returning person from cache: {}", person);
+        return person;
+    }
+
+    private Person getPersonFromSwapi(String id) {
         var url = urlCreator.createPersonByIdURL(id);
         String peopleAsString = httpService.sendSingleGetRequest(url);
 
@@ -37,5 +53,9 @@ public class PersonServiceImpl implements PersonService{
         }
         logger.info("Got response from SWAPI: {}", person);
         return person;
+    }
+
+    private Person getPersonFromCache(String id) {
+        return cacheService.get(id);
     }
 }
